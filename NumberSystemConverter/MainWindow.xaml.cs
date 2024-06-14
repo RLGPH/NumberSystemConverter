a06547a2a69fe08dc157238aabf61ec4d428e2c4
Casper Simon Jensen <casp142b@elev.techcollege.dk>
2024-06-14 08:05:12 +0200

    First commit with a working number system converter

---
+++ NumberSystemConverter/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NumberSystemConverter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    { 
+        NumberSystemConv numberSystemConv = new();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TB_Decimal_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string DecimalST = TB_Decimal.Text;
+            int Decimal;
+            if(int .TryParse(DecimalST, out Decimal))
+            {
+                
+                TB_Oktal.Text = numberSystemConv.DecimalToOctal(Decimal);
+                TB_Hexa.Text = numberSystemConv.DecimalToHexadecimal(Decimal);
+                TB_Binary.Text = numberSystemConv.DecimalToBinary(Decimal);
+            }
+            else
+            {
+                TB_Oktal.Text = null; 
+                TB_Hexa.Text = null;
+                TB_Binary.Text = null;
+            }
+        }
+
+        private void TB_Oktal_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string Oktal = TB_Oktal.Text;
+            (int? decimalNumber, string binaryNumber, string Hexanumber) = numberSystemConv.OctalToDecimal(Oktal);
+            TB_Decimal.Text = decimalNumber.ToString();
+            TB_Binary.Text = binaryNumber;
+            TB_Hexa.Text = Hexanumber;
+        }
+
+        private void TB_Binary_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string Binary = TB_Binary.Text;
+
+        }
+
+        private void TB_Hexa_TextChanged(object sender, TextChangedEventArgs e)
+        { 
+            string hexaST = TB_Hexa.Text;
+            (int? decimalNumber, string binaryNumber, string octalNumber) = numberSystemConv.HexadecimalToDecimal(hexaST);
+            TB_Decimal.Text = decimalNumber.ToString();
+            TB_Binary.Text = binaryNumber;
+            TB_Oktal.Text = octalNumber;
+        }
+    }
+}
---
+++ NumberSystemConverter/NumberSystemConv.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberSystemConverter
+{
+    public class NumberSystemConv
+    {
+        public string? DecimalToBinary(int decimalNumber)
+        {
+            if (decimalNumber > 0)
+            {
+                return Convert.ToString(decimalNumber, 2);
+            }
+            return null;
+        }
+
+        public string? DecimalToOctal(int decimalNumber)
+        {
+            if (decimalNumber > 0)
+            {
+                return Convert.ToString(decimalNumber, 8);
+            }
+            return null;
+        }
+
+        public string? DecimalToHexadecimal(int decimalNumber)
+        {
+            if(decimalNumber > 0) 
+            {
+                return Convert.ToString(decimalNumber, 16);
+            }
+            return null;
+        }
+
+        public (int?, string?, string?) BinaryToDecimal(string binaryNumber)
+        {
+            if(binaryNumber != null && binaryNumber != "")
+            {
+                int decimalNumber = Convert.ToInt32(binaryNumber, 2);
+                return(decimalNumber, DecimalToOctal(decimalNumber),DecimalToHexadecimal(decimalNumber));
+            }
+            return (null, null, null);
+        }
+
+        public (int?, string?, string?) OctalToDecimal(string octalNumber)
+        {
+            if(octalNumber != null && octalNumber != "")
+            {
+                int decimalNumber = Convert.ToInt32(octalNumber, 8);
+                return(decimalNumber, DecimalToBinary(decimalNumber),DecimalToHexadecimal(decimalNumber));
+            }
+            return (null, null, null);
+        }
+
+        public (int?, string?, string?) HexadecimalToDecimal(string hexadecimalNumber)
+        {
+            if (hexadecimalNumber != null && hexadecimalNumber != "")
+            {
+                int decimalNumber = Convert.ToInt32(hexadecimalNumber, 16);
+                return (decimalNumber, DecimalToBinary(decimalNumber), DecimalToOctal(decimalNumber));
+            }
+            return(null,null,null);
+        }
+    }
+}
